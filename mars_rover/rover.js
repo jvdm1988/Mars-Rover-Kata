cc6e59406715f873a7405fcd0e2f8ac1b18a7920
Jessicavandermarel <vandermareljessica@gmail.com>
2017-05-25 17:02:11 -0400

    done

--- mars_rover/rover.js
+++ mars_rover/rover.js
@@ -1,12 +1,76 @@
 var myRover = {
-  position: [0,0], 
+  position: [0,0],
   direction: 'N'
 };
+
+var planet = {
+  grid: (10,10,10,10)
+};
+
+if (planet => 10){
+  alert = "You're going off the grid";
+}
 
 function goForward(rover) {
   switch(rover.direction) {
     case 'N':
       rover.position[0]++
+      break;
+    case 'E':
+      rover.position[1]--
+      break;
+    case 'S':
+      rover.position[0]--
+      break;
+    case 'W':
+      rover.position[1]--
+      break;
+  };
+
+  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+}
+
+function goBackwards(rover){
+  switch (rover.direction){
+    case 'N':
+      rover.position[0]--
+      break;
+    case 'E':
+      rover.position[1]--
+      break;
+    case 'S':
+      rover.position[0]++
+      break;
+    case 'W':
+      rover.position[1]--
+      break;
+    };
+    console.log("New Rover position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+}
+
+function goLeft(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.position[0]--
+      break;
+    case 'E':
+      rover.position[1]--
+      break;
+    case 'S':
+      rover.position[0]--
+      break;
+    case 'W':
+      rover.position[1]++
+      break;
+  };
+
+  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+}
+
+function goRight(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.position[0]--
       break;
     case 'E':
       rover.position[1]++
@@ -22,4 +86,51 @@ function goForward(rover) {
   console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
 }
 
+
+function goRight(rover){
+  switch (rover.direction){
+  case 'N':
+    rover.direction='W';
+    break;
+  case 'E':
+    rover.direction='N';
+    break;
+  case 'S':
+    rover.direction='E';
+    break;
+  case 'W':
+    rover.direction='S';
+    break;
+  };
+  console.log("New Rover direction: [" + rover.position[0] + ", " + rover.position[1] + "]")
+}
+
+  function goLeft(rover){
+    switch (rover.direction){
+    case 'N':
+      rover.direction='E';
+      break;
+    case 'E':
+      rover.direction='S';
+      break;
+    case 'S':
+      rover.direction='W';
+      break;
+    case 'W':
+      rover.direction='N';
+      break;
+    };
+    console.log("New Rover direction: [" + rover.position[0] + ", " + rover.position[1] + "]")
+}
+
 goForward(myRover);
+goForward(myRover);
+goRight(myRover);
+goForward(myRover);
+goForward(myRover);
+goForward(myRover);
+goLeft(myRover);
+goForward(myRover);
+goForward(myRover);
+goBackwards(myRover);
+goBackwards(myRover);
